4c1b61bc838067b72ee58007924f1ff3a85434fb
Riadh Korchid <riadhkourchid@gmail.com>
2022-11-26 19:48:21 +0100

    create login page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {FcGoogle} from "react-icons/fc"
+import {AiFillFacebook} from "react-icons/ai"
+import {BsTwitter} from "react-icons/bs"
 export default function ()
 {
     return (
-        <h1> login page </h1>
+        <section className="login-section">
+                <div className="container">
+                    <input type="text"  placeholder="Enter email.."/>
+                    <input type="text" placeholder="Enter password.." />
+                    <a href="">forget password ?</a>
+                    <button>Sign in</button>
+                    <h1>don't have an account ?<a href="">Sign up!</a></h1>
+                    <h2>or sign in with </h2>
+                    <div className="icons">
+                         <FcGoogle  className="icon"/> <AiFillFacebook  className="icon" color="dodgerblue" /><BsTwitter className="icon" color="dodgerblue"/>
+                    </div>
+
+
+                </div>
+
+
+
+        </section>
     )
 }
